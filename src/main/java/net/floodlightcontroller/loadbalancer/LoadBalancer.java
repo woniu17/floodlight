61966943ecf955a2b242a004dc54d18b141a1996
woniu17 <qinglucklin@foxmail.com>
2015-05-19 15:32:56 +0800

    to make diff be more visiable in latex (cont.2)

--- src/main/java/net/floodlightcontroller/loadbalancer/LoadBalancer.java
+++ src/main/java/net/floodlightcontroller/loadbalancer/LoadBalancer.java
@@ -558,26 +558,37 @@ public class LoadBalancer implements IFloodlightModule,
 
                    if (sw.equals(pinSwitch.getId())) {
                        if (pinSwitch.getOFFactory().getVersion().compareTo(OFVersion.OF_12) < 0) { 
-                    	   actions.add(pinSwitch.getOFFactory().actions().setDlDst(MacAddress.of(member.macString)));
-                    	   actions.add(pinSwitch.getOFFactory().actions().setNwDst(IPv4Address.of(member.address)));
+                    	   actions.add(pinSwitch.getOFFactory().actions().
+                    			   setDlDst(MacAddress.of(member.macString)));
+                    	   actions.add(pinSwitch.getOFFactory().actions().
+                    			   setNwDst(IPv4Address.of(member.address)));
                     	   //add by qingluck, for port-mapping
-                    	   actions.add(pinSwitch.getOFFactory().actions().setTpDst(TransportPort.of(member.port))); 
-                    	   //actions.add(pinSwitch.getOFFactory().actions().setVlanVid(VlanVid.ofVlan(123))); //add by qingluck
-                    	   actions.add(pinSwitch.getOFFactory().actions().output(path.get(i+1).getPortId(), Integer.MAX_VALUE));
+                    	   actions.add(pinSwitch.getOFFactory().actions()
+                    			   .setTpDst(TransportPort.of(member.port))); 
+                    	   //actions.add(pinSwitch.getOFFactory().actions()
+                    	   //.setVlanVid(VlanVid.ofVlan(123))); //add by qingluck
+                    	   actions.add(pinSwitch.getOFFactory().actions().
+                    			   output(path.get(i+1).getPortId(), Integer.MAX_VALUE));
                        } else { // OXM introduced in OF1.2
-                    	   actions.add(pinSwitch.getOFFactory().actions().setField(pinSwitch.getOFFactory()
+                    	   actions.add(pinSwitch.getOFFactory()
+                    			   .actions().setField(pinSwitch.getOFFactory()
                     			   .oxms().ethDst(MacAddress.of(member.macString))));
-                    	   actions.add(pinSwitch.getOFFactory().actions().setField(pinSwitch.getOFFactory()
+                    	   actions.add(pinSwitch.getOFFactory()
+                    			   .actions().setField(pinSwitch.getOFFactory()
                     			   .oxms().ipv4Dst(IPv4Address.of(member.address))));
-                    	   actions.add(pinSwitch.getOFFactory().actions().setField(pinSwitch.getOFFactory()
+                    	   actions.add(pinSwitch.getOFFactory()
+                    			   .actions().setField(pinSwitch.getOFFactory()
                     			   .oxms().tcpDst(TransportPort.of(member.port)))); 
                     	   //add by qingluck, for port-mapping
-                    	   //actions.add(pinSwitch.getOFFactory().actions().setField(pinSwitch.getOFFactory()
+                    	   //actions.add(pinSwitch.getOFFactory().actions()
+                    	   //.setField(pinSwitch.getOFFactory()
                     	   //.oxms().vlanVid(OFVlanVidMatch.ofVlan(123))));
-                    	   actions.add(pinSwitch.getOFFactory().actions().output(path.get(i+1).getPortId(), Integer.MAX_VALUE));
+                    	   actions.add(pinSwitch.getOFFactory().actions()
+                    			   .output(path.get(i+1).getPortId(), Integer.MAX_VALUE));
                        }
                    } else {
-                	   actions.add(switchService.getSwitch(path.get(i+1).getNodeId()).getOFFactory().actions()
+                	   actions.add(switchService.getSwitch(path.get(i+1).getNodeId())
+                			   .getOFFactory().actions()
                 			   .output(path.get(i+1).getPortId(), Integer.MAX_VALUE));
                    }
                } else {
@@ -616,11 +627,15 @@ public class LoadBalancer implements IFloodlightModule,
                    
                    if (sw.equals(pinSwitch.getId())) {
                        if (pinSwitch.getOFFactory().getVersion().compareTo(OFVersion.OF_12) < 0) { 
-                    	   actions.add(pinSwitch.getOFFactory().actions().setDlSrc(vips.get(member.vipId).proxyMac));
-                    	   actions.add(pinSwitch.getOFFactory().actions().setNwSrc(IPv4Address.of(vips.get(member.vipId).address)));
+                    	   actions.add(pinSwitch.getOFFactory().actions()
+                    			   .setDlSrc(vips.get(member.vipId).proxyMac));
+                    	   actions.add(pinSwitch.getOFFactory().actions()
+                    			   .setNwSrc(IPv4Address.of(vips.get(member.vipId).address)));
                     	   //add by qingluck, for port-mapping
-                    	   actions.add(pinSwitch.getOFFactory().actions().setTpSrc(TransportPort.of(vips.get(member.vipId).port)));
-                    	   actions.add(pinSwitch.getOFFactory().actions().output(path.get(i+1).getPortId(), Integer.MAX_VALUE));
+                    	   actions.add(pinSwitch.getOFFactory().actions()
+                    			   .setTpSrc(TransportPort.of(vips.get(member.vipId).port)));
+                    	   actions.add(pinSwitch.getOFFactory().actions()
+                    			   .output(path.get(i+1).getPortId(), Integer.MAX_VALUE));
                        } else { // OXM introduced in OF1.2
                     	   actions.add(pinSwitch.getOFFactory().actions().setField(pinSwitch.getOFFactory()
                     			   .oxms().ethSrc(vips.get(member.vipId).proxyMac)));
@@ -629,10 +644,12 @@ public class LoadBalancer implements IFloodlightModule,
                     	   actions.add(pinSwitch.getOFFactory().actions().setField(pinSwitch.getOFFactory()
                     			 //add by qingluck, for port-mapping
                     			   .oxms().tcpSrc(TransportPort.of(vips.get(member.vipId).port))));
-                    	   actions.add(pinSwitch.getOFFactory().actions().output(path.get(i+1).getPortId(), Integer.MAX_VALUE));
+                    	   actions.add(pinSwitch.getOFFactory().actions()
+                    			   .output(path.get(i+1).getPortId(), Integer.MAX_VALUE));
                        }
                    } else {
-                	   actions.add(switchService.getSwitch(path.get(i+1).getNodeId()).getOFFactory().actions()
+                	   actions.add(switchService.getSwitch(path.get(i+1)
+                			   .getNodeId()).getOFFactory().actions()
                 			   .output(path.get(i+1).getPortId(), Integer.MAX_VALUE));
                    }
                    
